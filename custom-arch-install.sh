a76e8e346795654712254226402536f36d2e5c65
Krish Kansara <krishmkansara@gmail.com>
2025-07-09 03:50:44 -0400

    HOME partition creation and Minor Fixes

--- custom-arch-install.sh
+++ custom-arch-install.sh
@@ -1,12 +1,43 @@
 #!/bin/bash
 
+
 # v0.0.5
 # ToDO : Make home partition 
 
 
+set -euo pipefail
+
+if [[ $EUID -ne 0 ]]; then
+    echo "Please run as root"
+    exit 1
+fi
+
+# Create HOME partition using parted
+create_home_partition() {
+    HOME_SIZE="$(( $(parted "$DISK" unit MiB print free | awk '/Free Space/ {e=$2} END {gsub("MiB","",e); print e}') / 1024 ))G"
+    echo "Creating Home Partition of size $HOME_SIZE"
+    HOME_END="$(parted "$DISK" unit MiB print free | awk '/Free Space/ {e=$2} END {gsub("MiB","",e); print e"M"}')"
+
+    # Create HOMR partition using parted
+    parted --script "$DISK" mkpart primary ext4 "$ROOT_END" "$HOME_END"
+    parted --script "$DISK" name 3 "HOME"
+
+    # Determine partition name
+    if [[ $DISK =~ nvme ]]; then
+        HOME_PARTITION="${DISK}p3"  # NVMe uses p1, p2...
+    
+    else
+        HOME_PARTITION="${DISK}3"   # Standard disks use 1, 2...
+    
+    fi
+
+    echo "Home partition created at: $HOME_PARTITION"
+
+}
+
+# Create ROOT partition using parted
 create_root_partition() {
 
-    echo "Creating root partition..."
     while true; do
         if [[ "$SEPARATE_HOME_PARTITION" == "y" ]]; then
             ROOT_SIZE="120G"
@@ -27,6 +58,7 @@ create_root_partition() {
                 else
                     echo "Invalid size format! Please enter a number followed by 'M' (MB) or 'G' (GB) (e.g., 512M, 1G)."
                     sleep 3
+                    continue
                 fi
    
             else
@@ -37,8 +69,7 @@ create_root_partition() {
         elif [[ "$SEPARATE_HOME_PARTITION" == "n" ]]; then
             ROOT_END=$(parted "$DISK" unit MiB print free | awk '/Free Space/ {e=$2} END {gsub("MiB","",e); print e}')
             FREE_SIZE=$(parted "$DISK" unit MiB print free | awk '/Free Space/ {s=$3} END {gsub("MiB","",s); print s}')
-            ROOT_SIZE="$(( FREE_SIZE / 1024 ))G"
-            ROOT_END="${ROOT_END}M"
+            ROOT_SIZE="$(( (FREE_SIZE - ${EFI_END//M/}) / 1024 ))G"
             break
         fi
     done
@@ -47,6 +78,7 @@ create_root_partition() {
 
     # Create ROOT partition using parted
     parted --script "$DISK" mkpart primary ext4 "$EFI_END" "$ROOT_END"
+    parted --script "$DISK" name 2 "ROOT"
 
     # Determine partition name
     if [[ $DISK =~ nvme ]]; then
@@ -61,93 +93,90 @@ create_root_partition() {
 
 }
 
-# create_root_partition() {
-
-#     read -rp "Do you want to create a separate home partition? (y/n)" SEPARATE_HOME_PARTITION
-#     SEPARATE_HOME_PARTITION=$(echo "$SEPARATE_HOME_PARTITION" | tr '[:upper:]' '[:lower:]')      # Convert to lowercase
-
-#     # Creating Root partition of size 160GiB for maximum support
-#     if [[ SEPARATE_HOME_PARTITION == "y" ]]; then
-#         echo "Creating a root partition..."
-
-
-# }
-
 create_swap_file() {
 
-    RAM_SIZE=$(awk '/MemTotal/ {printf "%.0f", ($2 / 1024 / 1024) + 1}' /proc/meminfo)
+    echo "Creating SWAP partition..."
 
-    if [[ "$HIBERNATION_REQUIRED" == "y" ]]; then
-        SWAP_SIZE="${RAM_SIZE}G"
-    
-    elif [[ "$HIBERNATION_REQUIRED" == "n" ]];  then
-        if (( RAM_SIZE <= "2" )); then
-            SWAP_SIZE="4G"
-        elif (( RAM_SIZE <= "8" )); then
-            SWAP_SIZE="8G"
-        elif (( RAM_SIZE <= "16" )); then
-            SWAP_SIZE="16G"
-        elif (( RAM_SIZE <= "32" )); then
-            SWAP_SIZE="16G"
-        elif (( RAM_SIZE <= "48" )); then
-            SWAP_SIZE="24G"
+    while true; do
+        RAM_SIZE=$(awk '/MemTotal/ {printf "%.0f", ($2 / 1024 / 1024) + 1}' /proc/meminfo)
+
+        if [[ "$HIBERNATION_REQUIRED" == "y" ]]; then
+            SWAP_SIZE="${RAM_SIZE}G"
+            break
+        
+        elif [[ "$HIBERNATION_REQUIRED" == "n" ]];  then
+            if (( RAM_SIZE <= "2" )); then
+                SWAP_SIZE="4G"
+            elif (( RAM_SIZE <= "8" )); then
+                SWAP_SIZE="8G"
+            elif (( RAM_SIZE <= "16" )); then
+                SWAP_SIZE="16G"
+            elif (( RAM_SIZE <= "32" )); then
+                SWAP_SIZE="16G"
+            elif (( RAM_SIZE <= "48" )); then
+                SWAP_SIZE="24G"
+            else
+                SWAP_SIZE="32G"
+            fi
+            break
+        
         else
-            SWAP_SIZE="32G"
+            echo "Please enter correct input y or n."
+            sleep 3
+            continue
         fi
-    
-    else
-        echo "Please enter correct input y or n."
-        sleep 3
-        create_swap_file
-        return
-    fi
+    done
 
     echo "Creating swap partition of ${SWAP_SIZE%G} GB"
     
-    fallocate -l $SWAP_SIZE /mnt/swapfile
+    fallocate -l "$SWAP_SIZE" /mnt/swapfile
     chmod 600 /mnt/swapfile
     mkswap /mnt/swapfile
     swapon /mnt/swapfile
 
-    echo "/swapfile none swap sw 0 0" | tee -a /mnt/etc/fstab
+    #  echo "/swapfile none swap sw 0 0" | tee -a /mnt/etc/fstab
 }
 
+# Create EFI partition using parted
 create_efi_partition() {
 
-    local EFI_SIZE="1G"  # Default EFI size is 1G if not provided
-    local EFI_RESPONSE
+    EFI_SIZE="1G"  # Default EFI size is 1G if not provided
 
+    echo "Creating EFI partition..."
+    while true; do
 
-    read -rp "Do you wanna set a custom size of EFI Partition? (Default: 1G): " EFI_RESPONSE
-        
-    if [[ -n "$EFI_RESPONSE" ]]; then
+        read -rp "Do you wanna set a custom size of EFI Partition? (Default: 1G): " EFI_RESPONSE
+            
+        if [[ -n "$EFI_RESPONSE" ]]; then
 
-        if [[ "$EFI_RESPONSE" =~ ^[0-9]+M$ ]]; then
-            EFI_SIZE="$EFI_RESPONSE"
-            EFI_END="$EFI_RESPONSE"                        # User entered MB, no conversion needed
-        
-        elif [[ "$EFI_RESPONSE" =~ ^[0-9]+G$ ]]; then
-            EFI_SIZE="$EFI_RESPONSE"
-            EFI_END="$(( ${EFI_RESPONSE%G} * 1024 ))M"     # Convert GB to MB
-        
+            if [[ "$EFI_RESPONSE" =~ ^[0-9]+M$ ]]; then
+                EFI_SIZE="$EFI_RESPONSE"
+                EFI_END="$EFI_RESPONSE"                        # User entered MB, no conversion needed
+                break
+            
+            elif [[ "$EFI_RESPONSE" =~ ^[0-9]+G$ ]]; then
+                EFI_SIZE="$EFI_RESPONSE"
+                EFI_END="$(( ${EFI_RESPONSE%G} * 1024 ))M"     # Convert GB to MB
+                break
+            
+            else
+                echo "Invalid size format! Please enter a number followed by 'M' (MB) or 'G' (GB) (e.g., 512M, 1G)."
+                sleep 3
+                continue
+            fi
+
         else
-            echo "Invalid size format! Please enter a number followed by 'M' (MB) or 'G' (GB) (e.g., 512M, 1G)."
-            sleep 3
-            create_efi_partition
-            return
+            EFI_END="$(( ${EFI_SIZE%G} * 1024 ))M"              # Convert GB to MB
+        
         fi
-
-    else
-        EFI_END="$(( ${EFI_SIZE%G} * 1024 ))M"              # Convert GB to MB
-    
-    fi
+    done
 
     echo "Creating EFI Partition of size $EFI_SIZE"
 
     # Create EFI partition using parted
-    sudo parted --script "$DISK" mklabel gpt
-    sudo parted --script "$DISK" mkpart ESP fat32 1MiB "$EFI_END"
-    sudo parted --script "$DISK" set 1 esp on
+    parted --script "$DISK" mkpart ESP fat32 1MiB "$EFI_END"
+    parted --script "$DISK" set 1 esp on
+    parted --script "$DISK" name 1 "EFI"
 
     # Determine partition name
     if [[ $DISK =~ nvme ]]; then
@@ -167,7 +196,6 @@ partitioning() {
     echo "Starting auto/recommended partitioning..."
 
     # Creating EFI Partition
-    echo "Creating EFI Partition..."
     create_efi_partition
 
     # Option to create swap partition
@@ -205,12 +233,11 @@ partitioning() {
                 ;;
             n)
                 echo "Creating root partition..."
-                read -rp "Do you want a separate home partition? (y/n): " SEPARATE_HOME_PARTITION
+                read -rp "Do you want a home partition? (y/n): " SEPARATE_HOME_PARTITION
                 create_root_partition
-                read -rp "Do you want a home partition? (y/n): " HOME_CHOICE
-                HOME_CHOICE=$(echo "$HOME_CHOICE" | tr '[:upper:]' '[:lower:]')  # Convert to lowercase
+                SEPARATE_HOME_PARTITION=$(echo "$SEPARATE_HOME_PARTITION" | tr '[:upper:]' '[:lower:]')  # Convert to lowercase
                 while true; do
-                    case "$HOME_CHOICE" in
+                    case "$SEPARATE_HOME_PARTITION" in
                         y)
                             echo "Creating home partition..."
                             create_home_partition
@@ -240,7 +267,7 @@ wipe_disk() {
     echo "wiping $DISK..."
     
     # Create a new partition table (GPT format)
-    sudo parted --script "$DISK" mklabel gpt
+    parted --script "$DISK" mklabel gpt
 
     echo "Disk wiped successfully"
 }
@@ -264,12 +291,15 @@ disk_selection() {
                 # Get disk size
                 DISK_SIZE=$(parted "$DISK" unit MiB print | awk '/Disk/ {print $3}' | sed 's/MiB//')
                 break
+                ;;
             n)
                 echo "Aborted."
                 exit 1
+                ;;
             *)
                 echo "Please select correct input yes or no."
                 sleep 3
+                ;;
         esac
     done
 }
